e2e2d10a445f7469ae8092da268c46c339676b18
Mohamed Yasser <47388207+Midyass@users.noreply.github.com>
2021-04-21 11:25:17 +0200

    Update CE_bot.py

--- CE_bot.py
+++ CE_bot.py
@@ -7,4 +7,10 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+markup = types.ReplyKeyboardMarkup(row_width=2)
+itembtn1 = types.KeyboardButton('a')
+itembtn2 = types.KeyboardButton('v')
+itembtn3 = types.KeyboardButton('d')
+markup.add(itembtn1, itembtn2, itembtn3)
+tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
 bot.polling()

dfa6589665c2daa7325d4ec39a4dd7880cb442d2
Mohamed Yasser <47388207+Midyass@users.noreply.github.com>
2021-04-21 11:15:21 +0200

    Update CE_bot.py

--- CE_bot.py
+++ CE_bot.py
@@ -1,13 +1,10 @@
-import requests
+import telebot
 
-bot_key ='1526355880:AAGfr5_2SbmPn4nb38Prnj7r88-wQ52wH8o'
-chat_id='1109158839'
-
-def send_update(chat_id,msg):
-    url = f"https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={msg}"
-    requests.get(url)
-
-def main():
-    send_update(chat_id,"hi")
-
-main()
+bot = telebot.TeleBot("1526355880:AAGfr5_2SbmPn4nb38Prnj7r88-wQ52wH8o", parse_mode=None) 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+bot.polling()

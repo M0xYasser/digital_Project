f6c5001989dd30bc26e35f8af4ef938b21c5243c
Mohamed Yasser <47388207+Midyass@users.noreply.github.com>
2021-04-22 23:07:11 +0200

    Update CE_bot.py

--- CE_bot.py
+++ CE_bot.py
@@ -7,9 +7,9 @@ bot = telebot.TeleBot("1755271135:AAHb9TywoZ0v9YXIYpDaFAQ3kbRm8LZIPz0", parse_mo
 
 keyboardmain = types.InlineKeyboardMarkup(row_width=1)
 first_button = types.InlineKeyboardButton(text="Video 📹", url="https://telegra.ph/test-04-22-166")
-second_button = types.InlineKeyboardButton(text="PDF 📄", url="https://telegra.ph/test-04-22-166")
+second_button = types.InlineKeyboardButton(text="PDF 📄", url="https://docs.google.com/presentation/d/1v8fpayLBjPj-LJySIIK_kfA1P_LZj-tmhDGUTNI1DBw/edit?fbclid=IwAR1zgvjLvlQedIm5aPvD_sNFJve3gw639uGdrrqfk7jDJVTyXKSgT_lOfg4#slide=id.gbd136ac94e_7_75")
 t_button = types.InlineKeyboardButton(text="Task 🎯", url="https://telegra.ph/test-04-22-166")
-s_button = types.InlineKeyboardButton(text="Task Form ✔️", url="https://telegra.ph/test-04-22-166")
+s_button = types.InlineKeyboardButton(text="Task Form ✔️", url="https://docs.google.com/forms/d/e/1FAIpQLSdVncWE2rHrMwvF7nMCJlhQ8XB5vBtrq-l8ui3V-4yyR0ZjBQ/viewform?fbclid=IwAR1p4dnyWVJXT-rGSGnUZ3lWanemciCMYEwWFEP5U4KOvih3DEYl9nDvzyw")
 keyboardmain.add(first_button, second_button,t_button,s_button)
 
 

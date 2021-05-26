eca8f70e652dd428c4ed6d2d2c2388b50dd1a20a
Mohamed Yasser <47388207+Midyass@users.noreply.github.com>
2021-05-26 10:58:31 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -2,7 +2,7 @@
 import telebot
 from telebot import types
 import math
-API_TOKEN="1604884958:AAFPOebWNtOx_rauopqcYi0wXs_LLy0GRyE"
+API_TOKEN="1844883186:AAHS-wqGST-ryVd-FkVmWMzb6P0q9Xwa67w"
 bot = telebot.TeleBot(API_TOKEN, parse_mode=None) 
 
 

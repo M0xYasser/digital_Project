cd343f87546c367b817dfb2fda4f27c1dcaa9486
Mohamed Yasser <47388207+Midyass@users.noreply.github.com>
2021-05-28 19:00:25 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -6,7 +6,7 @@ API_TOKEN="1844883186:AAHS-wqGST-ryVd-FkVmWMzb6P0q9Xwa67w"
 bot = telebot.TeleBot(API_TOKEN, parse_mode=None) 
 
 
-@bot.message_handler(commands=['start','upgrade'],content_types=["text","audio","voice","image","sticker"])
+@bot.message_handler(content_types=["text","audio","voice","image","sticker"])
 def start(message):
     enstart="The bot is in maintenance to add a new solution method (J-K FLIP FLOP), and it will work again at 8 pm"
     bot.send_message(message.chat.id,enstart,parse_mode="MarkdownV2",reply_markup=keyboard)
